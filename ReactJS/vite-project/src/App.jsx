3838d4866df3c13da7857e4682fe40e6cc17a08a
afifaf13 <mdafifafrozfahim@gmail.com>
2024-10-19 20:33:57 +0600

    j

--- ReactJS/vite-project/src/App.jsx
+++ ReactJS/vite-project/src/App.jsx
@@ -1,23 +1,12 @@
 import React from 'react'
-import Demo from './Demo'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import ContractFrom from './components/ContractFrom'
-import Footer from './components/Footer'
 
 const App = () => {
+
+  const items=['afsda','dsdf','dhaka' ,'islamabad']
+
   return (
     <div>
 
-    <h1 className='h1' style={
-      {color:'red'}
-    }>Hello</h1>
-
-    
-    <h1>{3+8}</h1>
-    
-     
-   <button >submit</button>
 
     </div>
   )
