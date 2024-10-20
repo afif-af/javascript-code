317653d97d91233401bacc66b0ad4521706cdba0
afifaf13 <mdafifafrozfahim@gmail.com>
2024-10-20 08:20:40 +0600

    aps

--- ReactJS/vite-project/src/App.jsx
+++ ReactJS/vite-project/src/App.jsx
@@ -2,48 +2,69 @@ import React, { useState } from 'react';
 import Hero from './components/Hero';
 
 const App = () => {
-  const [list, setList] = useState([]);
-  const [item, setItem] = useState('');
+  const [formObj, setFormObj] = useState({ fName: "", lname: "", city: "", gender: "" });
 
-  const addToList = () => {
-    if (item) { // Check if the item is not empty
-      setList(prevList => [...prevList, item]); // Add the new item to the list
-      setItem(''); // Clear the input after adding
-    }
+  const inputOnChange = (property, value) => {
+    setFormObj(prevObj => ({
+      ...prevObj,
+      [property]: value
+    }));
   };
 
-  const remove = (index) => {
-    alert(index); // Alert the index of the item being removed
-    setList(prevList => prevList.filter((_, i) => i !== index)); // Remove item at the specified index
-  };
+  const formSubmit=(e)=>{
+    e.preDefault();
+    console.log(formObj);
+    alert(JSON.stringify(formObj))
+  }
 
   return (
     <div className="container">
-      <table>
-        <tbody>
-          {list.length !== 0 ? ( // Correct conditional rendering syntax
-            list.map((element, index) => (
-              <tr key={index}> {/* Adding a unique key for each item */}
-                <td>{element}</td>
-                <td>
-                  <button onClick={() => remove(index)}>Remove</button>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="2">No items in the list</td> {/* Display when the list is empty */}
-            </tr>
-          )}
-        </tbody>
-      </table>
-      <input
-        type="text"
-        value={item} // Controlled input to reflect current state
-        onChange={(e) => setItem(e.target.value)}
-        placeholder="Enter item"
-      />
-      <button className="btn btn-primary" onClick={addToList}>Click me</button>
+      <form>
+        <input 
+          onChange={(e) => inputOnChange("fName", e.target.value)} 
+          value={formObj.fName} 
+          placeholder='First name' 
+        /><br/>
+
+        <input 
+          onChange={(e) => inputOnChange("lname", e.target.value)} 
+          value={formObj.lname} 
+          placeholder='Last name' 
+        /><br/>
+
+        <select 
+          onChange={(e) => inputOnChange("city", e.target.value)} 
+          value={formObj.city} 
+          name="" 
+          id=""
+        >
+          <option value="">Choose</option>
+          <option value="Dhaka">Dhaka</option>
+          <option value="SHa">SHa</option>
+        </select>
+        <br />
+        
+        <label>
+          <input 
+            onChange={(e) => inputOnChange("gender", "Male")} 
+            checked={formObj.gender === "Male"} 
+            type='radio' 
+            name="gender" 
+          /> Male
+        </label>
+        
+        <label>
+          <input 
+            onChange={(e) => inputOnChange("gender", "Female")} 
+            checked={formObj.gender === "Female"} 
+            type="radio" 
+            name="gender" 
+          /> Female
+        </label>
+        <br />
+        
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 };
