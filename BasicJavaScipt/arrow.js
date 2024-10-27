2925b51611153a72c56d1cf09d658d19d452ba5f
afifaf13 <mdafifafrozfahim@gmail.com>
2024-10-27 22:22:51 +0600

    j

--- BasicJavaScipt/arrow.js
+++ BasicJavaScipt/arrow.js
@@ -3,3 +3,29 @@ function number(){
 
 }
 console.log(number())
+
+let num=(n)=>{
+    console.log(n)
+}
+
+num(13)
+console.log(9)
+
+
+
+var javascript = {
+    name: 'javasc',
+    libraries: ['react', 'angular'],
+    printLibraries: function () {
+        this.libraries.forEach((a) => {
+            console.log(`${this.name} loves ${a}`);
+        });
+    }
+};
+
+javascript.printLibraries();
+
+
+
+ 
+
