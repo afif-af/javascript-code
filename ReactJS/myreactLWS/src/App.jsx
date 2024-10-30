40a0ecdc5eac75ae3f0418f568ff4247d2444dd4
afifaf13 <mdafifafrozfahim@gmail.com>
2024-10-30 21:21:20 +0600

    fgd

--- ReactJS/myreactLWS/src/App.jsx
+++ ReactJS/myreactLWS/src/App.jsx
@@ -1,57 +1,31 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 const App =()=>{
 
-  let [FormObj,setFormObj]=useState({
-    fname:"",
-    lname:"",
-    city:"",
-    gender:""
-  });
+  let [Data,setData]=useState();
+   
+  useEffect(()=>{
+
+
+
+    (async()=>{
+
+     let res = await fetch('https://dummyjson.com/products/')
+
+     let json=await res.json()
+     setData(json)
+       
+    })()
+
   
-  const InputOnChange=(property,value)=>{
-    setFormObj(preObj=>({
-      ...preObj,
-      [property]:value
-
-    }));
-
-
-  };
-
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    console.log("from submitted",FormObj);
-    alert(JSON.stringify(FormObj))
-  };
-
+  },[])
+  
   return(
-    <div className="container ">
-      <form onSubmit={handleSubmit}>
 
-         
-        <input onChange={(e)=>{InputOnChange("fname",e.target.value)}} value={FormObj.fname} type="text" name="" id="" placeholder="FirstName" /> <br></br>
-
-        <input onChange={(e)=>{InputOnChange("lname",e.target.value)}} value={FormObj.lname} placeholder="LastName"/><br>
-        </br>
-
-        <select onChange={(e)=>{InputOnChange("city",e.target.value)}} value={FormObj.city}>
-          <option>Chose city</option>
-          <option value="dhaka">Dhaka</option>
-          <option value="mymensingh"> mymen</option>
-        </select>
-        <br></br>
-
-        <input onChange={(e)=>{InputOnChange("gender",'Male')}} checked={FormObj.gender==="Male"} type="radio" name ="gender"/>Male
-
-        <input onChange={(e)=>{InputOnChange("gender",'Female')}} checked={FormObj.gender=="Female"} type="radio" name="gender" />Female<br>
-        </br>
-
-
-        <button  type="submit"> Submit</button>
-      </form>         
-
+    <div>
+      
+         {JSON.stringify(Data)}
     </div>
   )
 }
--- ReactJS/myreactLWS/src/main.jsx
+++ ReactJS/myreactLWS/src/main.jsx
@@ -4,8 +4,7 @@ import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <App />
      
-  </StrictMode>,
 )
