0c6350026f03437948ed14558d87e9c40f3ab1d0
afifaf13 <mdafifafrozfahim@gmail.com>
2024-10-30 21:02:42 +0600

    f

--- ReactJS/myreactLWS/src/App.jsx
+++ ReactJS/myreactLWS/src/App.jsx
@@ -2,37 +2,54 @@ import { useRef, useState } from "react";
 
 
 const App =()=>{
-  
+
   let [FormObj,setFormObj]=useState({
     fname:"",
     lname:"",
-    city:"dhaka",
-    gender:"Female"
-  })
+    city:"",
+    gender:""
+  });
   
+  const InputOnChange=(property,value)=>{
+    setFormObj(preObj=>({
+      ...preObj,
+      [property]:value
+
+    }));
+
+
+  };
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log("from submitted",FormObj);
+    alert(JSON.stringify(FormObj))
+  };
+
   return(
     <div className="container ">
-      <form action="#">
+      <form onSubmit={handleSubmit}>
 
          
-        <input  value={FormObj.fname} type="text" name="" id="" placeholder="FirstName" /> <br></br>
+        <input onChange={(e)=>{InputOnChange("fname",e.target.value)}} value={FormObj.fname} type="text" name="" id="" placeholder="FirstName" /> <br></br>
 
-        <input value={FormObj.lname} placeholder="LastName"/><br>
+        <input onChange={(e)=>{InputOnChange("lname",e.target.value)}} value={FormObj.lname} placeholder="LastName"/><br>
         </br>
 
-        <select value={FormObj.city}>
+        <select onChange={(e)=>{InputOnChange("city",e.target.value)}} value={FormObj.city}>
           <option>Chose city</option>
           <option value="dhaka">Dhaka</option>
           <option value="mymensingh"> mymen</option>
         </select>
         <br></br>
 
-        <input checked={FormObj.gender==="Male"} type="radio" name ="gender"/>Male
-        <input checked={FormObj.gender=="Female"} type="radio" name="gender" />Female<br>
+        <input onChange={(e)=>{InputOnChange("gender",'Male')}} checked={FormObj.gender==="Male"} type="radio" name ="gender"/>Male
+
+        <input onChange={(e)=>{InputOnChange("gender",'Female')}} checked={FormObj.gender=="Female"} type="radio" name="gender" />Female<br>
         </br>
 
 
-        <button type="submit"> Submit</button>
+        <button  type="submit"> Submit</button>
       </form>         
 
     </div>
